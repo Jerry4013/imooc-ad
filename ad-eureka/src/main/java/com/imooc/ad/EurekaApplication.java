5033ae8e1dfae9108121a16365ac38b12787e5a0
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-07-23 09:09:26 -0400

    ad-gateway

---
+++ ad-eureka/src/main/java/com/imooc/ad/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Jingchao Zhang
+ * Date: 2020-07-22
+ * Time: 8:46 PM
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
