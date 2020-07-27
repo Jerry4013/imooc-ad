80c49846ecddaf10f6c9368921eb31aa110dda3f
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-07-27 19:49:08 -0400

    entities

---
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/constant/CreativeType.java
@@ -0,0 +1,23 @@
+package com.imooc.ad.constant;
+
+import lombok.Getter;
+
+/**
+ * Created by Jingchao Zhang
+ * Date: 2020-07-27
+ * Time: 7:44 PM
+ */
+@Getter
+public enum CreativeType {
+    IMAGE(1, "图片"),
+    VIDEO(2, "视频"),
+    TEXT(3, "文本");
+
+    private int type;
+    private String desc;
+
+    CreativeType(int type, String desc) {
+        this.type = type;
+        this.desc = desc;
+    }
+}
---
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/entity/Creative.java
@@ -0,0 +1,81 @@
+package com.imooc.ad.entity;
+
+import com.sun.tools.javac.jvm.Gen;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Created by Jingchao Zhang
+ * Date: 2020-07-27
+ * Time: 7:31 PM
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ad_creative")
+public class Creative {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Basic
+    @Column(name = "type", nullable = false)
+    private Integer type;
+
+    @Basic
+    @Column(name = "material_type", nullable = false)
+    private Integer materialType;
+
+    @Basic
+    @Column(name = "height", nullable = false)
+    private Integer height;
+
+    @Basic
+    @Column(name = "width", nullable = false)
+    private Integer width;
+
+    @Basic
+    @Column(name = "size", nullable = false)
+    private Long size;
+
+    @Basic
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    @Basic
+    @Column(name = "audit_status", nullable = false)
+    private Integer auditStatus;
+
+    @Basic
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Basic
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Basic
+    @Column(name = "create_time", nullable = false)
+    private Date createTime;
+
+    @Basic
+    @Column(name = "update_time", nullable = false)
+    private Date updateTime;
+}
