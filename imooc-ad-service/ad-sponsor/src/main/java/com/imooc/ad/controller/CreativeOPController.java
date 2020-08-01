416b051b39c3f1895cc3d569c382b414387e1c3f
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-07-31 23:21:11 -0400

    controllers

---
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/controller/CreativeOPController.java
@@ -0,0 +1,32 @@
+package com.imooc.ad.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.ad.service.ICreativeService;
+import com.imooc.ad.vo.CreativeRequest;
+import com.imooc.ad.vo.CreativeResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Jingchao Zhang
+ * Date: 2020-07-31
+ * Time: 11:14 PM
+ */
+@Slf4j
+@RestController
+public class CreativeOPController {
+
+    private final ICreativeService creativeService;
+
+    public CreativeOPController(ICreativeService creativeService) {
+        this.creativeService = creativeService;
+    }
+
+    @PostMapping("/create/creative")
+    public CreativeResponse createCreative(@RequestBody CreativeRequest request) {
+        log.info("ad-sponsor: createCreative -> {}", JSON.toJSONString(request));
+        return creativeService.createCreative(request);
+    }
+}
