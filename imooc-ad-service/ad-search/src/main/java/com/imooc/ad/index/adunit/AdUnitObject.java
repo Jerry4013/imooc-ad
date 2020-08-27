69dd5119f691de48b56134d207b66fe4f547b0f9
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-08-27 17:58:12 -0400

    index

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/adunit/AdUnitObject.java
@@ -0,0 +1,42 @@
+package com.imooc.ad.index.adunit;
+
+import com.imooc.ad.index.adplan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Jingchao Zhang
+ * Date: 2020-08-27
+ * Time: 5:46 PM
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitObject {
+
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject) {
+        if (newObject.getUnitId() != null) {
+            this.unitId = newObject.getUnitId();
+        }
+        if (newObject.getUnitStatus() != null) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+        if (newObject.getPositionType() != null) {
+            this.positionType = newObject.getPositionType();
+        }
+        if (newObject.getPlanId() != null) {
+            this.planId = newObject.getPlanId();
+        }
+        if (newObject.getAdPlanObject() != null) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+}
